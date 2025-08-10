f2911b8726354289d9fe3e30287628f093f64582
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-10 18:48:44 +0330

    Building clean architecture

---
+++ JustDoIt.Presentation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JustDoIt.Presentation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JustDoIt.Presentation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JustDoIt.Presentation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
